f002455b6381602034445ba4465333351696cb80
armando <armandomendivil.m@gmail.com>
2017-11-20 13:14:47 -0700

    Add real time

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
-app.use(cors())
+app.use(cors());
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var posts = [
-  { title: 'POST 1', content: 'post 1Lorem ipsum dolor sit amet' },
-  { title: 'POST 2', content: 'p2 Lorem ipsum dolor sit amet' },
-  { title: 'POST 3', content: 'p3 - Lorem ipsum dolor sit amet' },
-  { title: 'POST 4', content: 'p4 - Lorem ipsum dolor sit amet' }
+  { title: 'ANONIMO', content: 'post 1Lorem ipsum dolor sit amet' },
+  { title: 'ANONIMO', content: 'p2 Lorem ipsum dolor sit amet' },
+  { title: 'ANONIMO', content: 'p3 - Lorem ipsum dolor sit amet' },
+  { title: 'ANONIMO', content: 'p4 - Lorem ipsum dolor sit amet' }
 ];
 
 app.get('/posts', function (req, res) {
   res.send({ posts: posts });
 });
 
-app.listen(9000, function () {
+io.on('connection', function(socket){
+  socket.on('post', function(msg){
+    console.log(msg);
+    io.emit('post', msg);
+  });
+});
+
+server.listen(9000, function () {
   console.log('Example app listening on port 9000!');
 });
